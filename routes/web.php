e5b5e0fb487b29dc6598cf2319bc81e6c49ec3b1
markl180x <marklloyd.elarmo@gmail.com>
2024-07-23 14:42:36 +0800

    add code

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('pricing', function () {
+    return view('pricing');
+});
+
+Route::get('/faq', function () {
+    return view('faq');
+});
+
+Route::get('/bloghome', function () {
+    return view('bloghome');
+});
+
+Route::get('/blogpost', function () {
+    return view('blogpost');
+});
+
+Route::get('/portfoliooverview', function () {
+    return view('/portfoliooverview');
+});
+
+Route::get('/portfolioitem', function () {
+    return view('/portfolioitem');
+});
